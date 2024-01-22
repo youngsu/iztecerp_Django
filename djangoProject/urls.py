0245cf389f71674b4e8cf7c20c3709636806e74f
ysp <kazu0073@naver.com>
2024-01-22 18:17:51 +0900

    Initial commit

---
+++ djangoProject/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for djangoProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views  # 현재 프로젝트의 views 모듈을 가져옵니다.
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # 루트 URL에 index 뷰를 연결합니다.
+]
---
+++ djangoProject/views.py
@@ -0,0 +1,5 @@
+# 앱의 views.py 파일
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
